140300eb3caa693d9e5e56dd818452ba32a68cea
xoxoedg <rugyasin@gmail.com>
2022-04-24 12:55:18 +0200

    Changed lookup dto and adapter

--- src/administration/lookup/lookup_adapter.py
+++ src/administration/lookup/lookup_adapter.py
@@ -6,6 +6,7 @@ class LookupAdapter:
     @staticmethod
     def to_dto(lookup):
         lock_up_dto = LookupDto()
+        lock_up_dto.id = lookup.id
         lock_up_dto.app_name = lookup.app_name
         lock_up_dto.api_name = lookup.api_name
 
--- src/administration/lookup/lookup_dto.py
+++ src/administration/lookup/lookup_dto.py
@@ -1,10 +1,12 @@
 class LookupDto:
 
+    id: int = 0
     app_name: str = ""
     api_name: str = ""
 
     def serialize(self) -> dict:
         return {
+            "id": self.id,
             "app_name": self.app_name,
             "api_name": self.api_name
         }
